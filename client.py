d46a5c30d4297ffd6e75e4ceed2d4a7e7e60df50
kelvin-de-oliveira <kelvin.oliveira.0110@gmail.com>
2026-04-26 20:19:10 -0300

    resolucao questao 4

--- client.py
+++ client.py
@@ -11,3 +11,11 @@ if not printer:
 printer.printString("Hello World!")
 printer.printUpperCase("Hello World!")
 printer.printReverse("Hello World!")
+
+base2 = communicator.stringToProxy("SimpleGreeter:default -p 11000")
+greeter = Demo.GreeterPrx.checkedCast(base2)
+if not greeter:
+    raise RuntimeError("Invalid proxy")
+
+greeter.greet("Kelvin")
+greeter.farewell("Kelvin")
